0f52675fe0be725ca3311a101c046ea11d42c12c
KevinTS450 <kevinmaharo450@gmail.com>
2025-04-15 17:07:16 +0200

    feat(OBL): Import student

--- routes/_API/studentAPI.js
+++ routes/_API/studentAPI.js
@@ -2,11 +2,15 @@ const express =require("express")
 
 const route = express.Router();  // Create a router instance
 const studentController = require("../../controller/studentController")
+const multer = require('multer');
+
+const upload = multer({ dest: 'uploads/' });
 
 
 route.post('/', studentController.updateOrCreate);
 route.get("/" ,studentController.getAll)
 route.get("/getById" ,studentController.getById)
+route.post("/import" , upload.single('file'),studentController.Import)
 route.delete("/" , studentController.destroy)
 
 module.exports = route;
